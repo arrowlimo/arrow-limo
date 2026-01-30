ce71e340a3177cd578bd4359a766070791f90807
Arrow Limousine <info@arrowlimousine.ca>
2026-01-30 13:38:05 -0700

    SECURITY: Remove customer payment data with PII (emails, card numbers, amounts)

--- scripts/check_empty_names.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host='localhost', database='almsdata', user='postgres', password='***REDACTED***')
-cur = conn.cursor()
-cur.execute("SELECT employee_id, employee_number, full_name FROM employees WHERE employee_number IN ('Dr114', 'Dr128', 'DR114', 'DR128') ORDER BY employee_id")
-for r in cur.fetchall():
-    print(f"ID: {r[0]}, Number: {r[1]}, Name: [{r[2]}]")
-cur.close()
-conn.close()
